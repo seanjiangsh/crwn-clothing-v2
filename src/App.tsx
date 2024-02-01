efbd657760a807d499cda575f3e00fe4d1e1c62e
sean <seanjiangsh@gmail.com>
2024-02-01 08:54:34 +0800

    adjusted routes and styling for Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
-import Navigation from "./components/navigation/Navigation";
-import Home from "./routes/home/home";
+import Navigation from "./routes/navigation/Navigation";
+import Home from "./routes/home/Home";
 
 function Shop() {
   return <h1>Shop page</h1>;
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        <Route path="/home" element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="/shop" element={<Shop />} />
       </Route>
     </Routes>
---
+++ src/routes/navigation/Navigation.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./Navigation.css";
+
+export default function Navigation() {
+  return (
+    <React.Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </React.Fragment>
+  );
+}
