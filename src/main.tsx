1c1448c5eded9ee8be969fc7f88d0766b526b70b
sean <seanjiangsh@gmail.com>
2024-02-19 19:25:43 +0800

    added status persistent feature via redux-persist

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 import { StyleSheetManager } from "styled-components";
 
-import { store } from "./redux/store.ts";
+import { persister, store } from "./redux/store.ts";
 
 import App from "./App.tsx";
 import "./index.css";
@@ -12,11 +13,13 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <StyleSheetManager shouldForwardProp={() => true}>
-          <App />
-        </StyleSheetManager>
-      </BrowserRouter>
+      <PersistGate persistor={persister}>
+        <BrowserRouter>
+          <StyleSheetManager shouldForwardProp={() => true}>
+            <App />
+          </StyleSheetManager>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,12 +4,27 @@ import {
   applyMiddleware,
 } from "redux";
 import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
-const middlewares = [logger];
-const composeEnhancers = compose(applyMiddleware(...middlewares));
+const middlewares = import.meta.env.DEV ? [logger] : [];
 
-export const store = createStore(rootReducer, undefined, composeEnhancers);
+const composeEnhancer =
+  (!import.meta.env.PROD &&
+    window &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+const composeEnhancers = composeEnhancer(applyMiddleware(...middlewares));
+
+const persistedReducer = persistReducer(
+  { key: "root", storage, blacklist: ["user"] },
+  rootReducer as any, // tmp fix
+);
+
+export const store = createStore(persistedReducer, undefined, composeEnhancers);
+
+export const persister = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
