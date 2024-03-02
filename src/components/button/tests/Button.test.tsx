848999908cdb80eb897a4e7fed292e0f8f5dd345
sean <seanjiangsh@gmail.com>
2024-03-02 09:29:34 +0800

    initial setup for testing via vitest

---
+++ src/components/button/tests/Button.test.tsx
@@ -0,0 +1,46 @@
+import { expect, describe, it, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import Button from "../Button";
+
+describe("Button component", () => {
+  const user = userEvent.setup();
+
+  it("renders the base button when nothing is passed", () => {
+    render(<Button />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    // const styles = getComputedStyle(button);
+    // console.log({ backgroundColor: styles.backgroundColor });
+    expect(button).toHaveAttribute("button-type", "");
+  });
+
+  it("renders the google-sign-in button type", () => {
+    render(<Button buttonType="google-sign-in" />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveAttribute("button-type", "google-sign-in");
+  });
+
+  it("renders the inverted button type", () => {
+    render(<Button buttonType="inverted" />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveAttribute("button-type", "inverted");
+  });
+
+  it("renders a loading spinner when isLoading is true", () => {
+    render(<Button isLoading>Loading button</Button>);
+    const spinner = screen.getByTestId("spinner");
+    expect(spinner).toBeInTheDocument();
+  });
+
+  it("calls the onClick handler when clicked", async () => {
+    const onClickMock = vi.fn();
+    render(<Button onClick={onClickMock} />);
+    const button = screen.getByRole("button");
+    await user.click(button);
+    expect(onClickMock).toHaveBeenCalled();
+  });
+});
